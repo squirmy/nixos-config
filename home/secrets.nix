5f40868ab1ea025daf75349d33cfbeee4f4a76a2
Adam Woods <squirmy.dev@gmail.com>
2023-08-14 13:10:42 +1000

    docs: add descriptions to secret packages

--- home/secrets.nix
+++ home/secrets.nix
@@ -5,18 +5,34 @@
   # to decrypt the secrets + yubikey agent for ssh auth.
   # https://words.filippo.io/dispatches/passage/
   home.packages = with pkgs; [
+    # age
+    # A simple, modern and secure encryption tool with small explicit keys,
+    # no config options, and UNIX-style composability.
     # https://github.com/FiloSottile/age
     age
+
+    # age-plugin-yubikey 
+    # Enables files to be encrypted to age identities stored on YubiKeys.
     # https://github.com/str4d/age-plugin-yubikey
     age-plugin-yubikey 
+
+    # passage
+    # A fork of password-store that uses age as backend.
     # https://github.com/FiloSottile/passage
     passage
+
+    # yubikey-agent
+    # A seamless ssh-agent for YubiKeys.
     # https://github.com/FiloSottile/yubikey-agent
     yubikey-agent
+
+    # YubiKey Manager
+    # Cross-platform application for configuring any YubiKey over all USB interfaces
     # https://www.yubico.com/support/download/yubikey-manager/
     yubikey-manager
   ];
 
+  # Configure the yubikey-agent service
   launchd.agents.yubikey-agent = {
     enable = true;
     config.KeepAlive = true;

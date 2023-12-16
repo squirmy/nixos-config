12fc518e478fb4ec87561767bc4d477553c1b28a
Adam Woods <squirmy.dev@gmail.com>
2023-12-16 18:28:16 +1100

    feat: add safari and auto update preferences

--- nix-darwin/macos.nix
+++ nix-darwin/macos.nix
@@ -23,6 +23,51 @@
 
   system.defaults.dock.appswitcher-all-displays = true;
 
+  system.defaults.CustomUserPreferences = {
+    NSGlobalDomain.WebKitDeveloperExtras = true;
+
+    "com.apple.Safari" = {
+      # Don’t send search queries to Apple
+      UniversalSearchEnabled = false;
+      SuppressSearchSuggestions = true;
+      AutoOpenSafeDownloads = false;
+
+      # Enable debug menu, developer menu and web inspector
+      IncludeInternalDebugMenu = true;
+      IncludeDevelopMenu = true;
+      WebKitDeveloperExtrasEnabledPreferenceKey = true;
+      "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" = true;
+
+      # Spell check, but don't correct
+      WebContinuousSpellCheckingEnabled = true;
+      WebAutomaticSpellingCorrectionEnabled = false;
+
+      # Disable AutoFill
+      AutoFillPasswords = false;
+      AutoFillFromAddressBook = false;
+      AutoFillCreditCardData = false;
+      AutoFillMiscellaneousForms = false;
+    };
+
+    "com.apple.SoftwareUpdate" = {
+      AutomaticCheckEnabled = true;
+      # Check for software updates daily, not just once per week
+      ScheduleFrequency = 1;
+      # Download newly available updates in background
+      AutomaticDownload = 1;
+      # Install System data files & security updates
+      CriticalUpdateInstall = 1;
+    };
+
+    # Turn on app auto-update
+    "com.apple.commerce".AutoUpdate = true;
+  };
+
+  system.activationScripts.postUserActivation.text = ''
+    # Following line should allow us to avoid a logout/login cycle
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
+
   # Set the user's name & home directory. This should be
   # in sync with home manager. (home/default.nix)
   users.users.${flake.config.myself.username} = {

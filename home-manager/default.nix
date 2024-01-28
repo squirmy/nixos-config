37a7121cd809ad578fd51272fef45cc79af6ac1b
Adam Woods <squirmy.dev@gmail.com>
2024-01-29 01:16:06 +1100

    refactor: move home.stateVersion into nix-machine

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,9 +1,4 @@
 {...}: {
-  # It is occasionally necessary for Home Manager to change configuration
-  # defaults in a way that is incompatible with stateful data.
-  # Update this when you want to consume newer defaults.
-  home.stateVersion = "22.11";
-
   # Programs and packages to manage with home manager.
   imports = [
     ./trampoline
--- lib/default.nix
+++ lib/default.nix
@@ -87,6 +87,11 @@ in {
                 home-manager.users.${machine.user.username} = {pkgs, ...}: {
                   imports = [
                     {
+                      # It is occasionally necessary for Home Manager to change configuration
+                      # defaults in a way that is incompatible with stateful data.
+                      # Update this when you want to consume newer defaults.
+                      home.stateVersion = "22.11";
+
                       # Set the username & home directory. This should be
                       # in sync with nix-darwin. (below)
                       home.username = machine.user.username;

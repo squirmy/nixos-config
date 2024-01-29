f46e6f36294402fa4d44ce3f19f2d35af7ce7f30
Adam Woods <squirmy.dev@gmail.com>
2024-01-29 23:57:09 +1100

    chore: expose a flake module

--- flake.nix
+++ flake.nix
@@ -12,5 +12,9 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["aarch64-darwin" "x86_64-darwin"];
       imports = [./lib ./catalog ./machines];
+
+      flake = {
+        flakeModule = ./catalog;
+      };
     };
 }

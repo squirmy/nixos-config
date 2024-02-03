da152476e45cc1203f308d67637c617dcf515757
Adam Woods <squirmy.dev@gmail.com>
2024-02-03 16:21:35 +1100

    chore: simplify nix-machine; home-manager is always enabled

--- lib/default.nix
+++ lib/default.nix
@@ -17,29 +17,6 @@
   nixDarwinModules = {imports = allCatalogOptions ++ allNixDarwinModules;};
   homeManagerModules = {imports = allCatalogOptions ++ allHomeManagerModules;};
 
-  specialArgsFor = rec {
-    common = {
-      inherit inputs;
-    };
-    nixos = common;
-    darwin =
-      common
-      // {
-        rosettaPkgs = import inputs.nixpkgs {system = "x86_64-darwin";};
-      };
-  };
-
-  macosMachineOptions = {
-    # I'm assuming that home-manager is going to be wanted most of the time
-    # so enabling it by default.
-    home-manager.enable = lib.mkOption {
-      default = true;
-      example = false;
-      description = "Whether to enable home-manager.";
-      type = lib.types.bool;
-    };
-  };
-
   catalogOptions = {
     options = lib.mkOption {
       type = lib.types.deferredModule;
@@ -62,7 +39,7 @@ in {
 
   options.nix-machine.macos = lib.mkOption {
     type = lib.types.attrsOf (lib.types.submoduleWith {
-      modules = allCatalogOptions ++ [{options = macosMachineOptions;}];
+      modules = allCatalogOptions;
     });
     default = {};
   };
@@ -73,33 +50,30 @@ in {
     flake = {
       darwinConfigurations =
         builtins.mapAttrs (
-          _machine-name: machine: let
-            # Allow access to the user
-            specialArgs = specialArgsFor.darwin;
-
-            # Remove non-catalog options from the machine config
-            options = builtins.removeAttrs machine (builtins.attrNames macosMachineOptions);
+          _machine-name: options: let
+            # allow nix-darwin and home-manager catalog modules to access inputs
+            specialArgs = {
+              inherit inputs;
+              rosettaPkgs = import inputs.nixpkgs {system = "x86_64-darwin";};
+            };
 
             # nix-darwin configuration
             nix-darwin-modules = [nixDarwinModules options];
 
             # home-manager configuration
-            home-manager-modules = lib.lists.optionals machine.home-manager.enable [
+            home-manager-modules = [
               inputs.home-manager.darwinModules.home-manager
               ({config, ...}: {
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
                 home-manager.extraSpecialArgs = specialArgs;
                 home-manager.users.${config.nix-machine.username} = {pkgs, ...}: {
-                  imports = [
-                    homeManagerModules
-                    options
-                  ];
+                  imports = [homeManagerModules options];
                 };
               })
             ];
           in (inputs.nix-darwin.lib.darwinSystem {
-            specialArgs = specialArgs;
+            inherit specialArgs;
             modules = nix-darwin-modules ++ home-manager-modules;
           })
         )

f71d83512bd3ab8759bc17228a993d7b1254a080
Adam Woods <squirmy.dev@gmail.com>
2024-02-04 14:31:57 +1100

    chore: tidy up homebrew

--- catalog/nix-darwin/homebrew.nix
+++ catalog/nix-darwin/homebrew.nix
@@ -5,17 +5,10 @@
 }:
 lib.mkIf config.squirmy.homebrew.enable {
   homebrew.enable = true;
-  homebrew.taps = [
-    "nrlquaker/createzap"
-  ];
+  homebrew.taps = ["nrlquaker/createzap"];
 
   homebrew.onActivation.autoUpdate = true;
   homebrew.onActivation.cleanup = "zap";
-
-  # Required so that applications installed with homebrew are in your path.
-  programs.zsh.shellInit = ''
-    eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
-  '';
 
   # Upgrade outdated mac app store apps
   homebrew.onActivation.upgrade = true;
--- catalog/nix-darwin/shell.nix
+++ catalog/nix-darwin/shell.nix
@@ -12,4 +12,9 @@ lib.mkIf config.squirmy.shell.enable {
 
   # Manage .zshrc. Required for packages to be in your path.
   programs.zsh.enable = true;
+
+  # Required so that applications installed with homebrew are in your path.
+  programs.zsh.shellInit = lib.mkIf config.squirmy.homebrew.enable ''
+    eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
+  '';
 }

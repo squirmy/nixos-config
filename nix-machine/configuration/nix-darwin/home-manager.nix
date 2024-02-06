cc096dd528cf9b1030f97b6132a73d2874ac77e6
Adam Woods <squirmy.dev@gmail.com>
2024-02-07 08:43:39 +1100

    chore: tidy up

--- nix-machine/configuration/nix-darwin/home-manager.nix
+++ nix-machine/configuration/nix-darwin/home-manager.nix
@@ -5,5 +5,7 @@
 }: {
   home-manager.useGlobalPkgs = lib.mkDefault true;
   home-manager.useUserPackages = lib.mkDefault true;
+
+  # allow home-manager modules to access inputs
   home-manager.extraSpecialArgs = import ../../lib/special-args.nix {inherit inputs;};
 }
--- nix-machine/flake-module.nix
+++ nix-machine/flake-module.nix
@@ -8,14 +8,14 @@
 
   configurations = map (x: x.value) (lib.attrsets.attrsToList nix-machine.configurations);
 
-  yyy = lib.catAttrs "options" configurations;
+  sharedOptions = lib.catAttrs "options" configurations;
   nixDarwinConfigurations = lib.catAttrs "nixDarwin" configurations;
   homeManagerConfigurations = lib.catAttrs "homeManager" configurations;
 
   # Share the options between nix-darwin and home-manager so that they can be
   # configured in a way that is agnostic to the where the configuration applies.
-  nixDarwinConfiguration = {imports = yyy ++ nixDarwinConfigurations;};
-  homeManagerConfiguration = {imports = yyy ++ homeManagerConfigurations;};
+  nixDarwinConfiguration = {imports = sharedOptions ++ nixDarwinConfigurations;};
+  homeManagerConfiguration = {imports = sharedOptions ++ homeManagerConfigurations;};
 
   configurationOptions = {
     options = lib.mkOption {
@@ -39,7 +39,7 @@ in {
 
   options.nix-machine.macos = lib.mkOption {
     type = lib.types.attrsOf (lib.types.submoduleWith {
-      modules = yyy;
+      modules = sharedOptions;
     });
     default = {};
   };

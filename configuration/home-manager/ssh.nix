5a816beeb82cdd84152d3e3676d5b4d5a07f2214
Adam Woods <squirmy.dev@gmail.com>
2024-03-04 20:18:35 +1100

    feat: use openssh fido2 resident key; remove yubikey-agent

--- configuration/home-manager/ssh.nix
+++ configuration/home-manager/ssh.nix
@@ -1,9 +1,14 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 lib.mkIf config.squirmy.ssh.enable {
   # Enable ssh. Not sure how much to setup with this just yet.
   programs.ssh.enable = true;
+
+  home.packages = [
+    pkgs.openssh
+  ];
 }
--- configuration/home-manager/yubikey.nix
+++ configuration/home-manager/yubikey.nix
@@ -3,32 +3,54 @@
   lib,
   config,
   ...
-}:
-lib.mkIf config.squirmy.yubikey.enable {
-  home.packages = with pkgs; [
-    # yubikey-agent
-    # A seamless ssh-agent for YubiKeys.
-    # https://github.com/FiloSottile/yubikey-agent
-    yubikey-agent
+}: let
+  buildPythonPackage = pkgs.python311Packages.buildPythonPackage;
 
-    # YubiKey Manager
-    # Cross-platform application for configuring any YubiKey over all USB interfaces
-    # https://www.yubico.com/support/download/yubikey-manager/
-    yubikey-manager
-  ];
+  openssh-key-parser = {
+    setuptools-scm,
+    bcrypt,
+    cryptography,
+    ...
+  }:
+    buildPythonPackage {
+      pname = "openssh-key-parser";
+      version = "0.0.7";
+      format = "pyproject";
 
-  # Configure the yubikey-agent service
-  launchd.agents.yubikey-agent = {
-    enable = true;
-    config.KeepAlive = true;
-    config.Label = "yubikey-agent";
-    config.ProgramArguments = [
-      "${pkgs.yubikey-agent}/bin/yubikey-agent"
-      "-l"
-      "/tmp/yubikey-agent.sock"
+      src = pkgs.fetchFromGitHub {
+        owner = "scottcwang";
+        repo = "openssh_key_parser";
+        rev = "1e35d9f1254e617eb3a35cfc9eee67f36944d30b";
+        sha256 = "sha256-cAPQwrRs+5HGmwG4wF5HiIFPcVQvr99M79rbgM6e6xg=";
+      };
+
+      nativeBuildInputs = [
+        setuptools-scm
+      ];
+
+      propagatedBuildInputs = [
+        bcrypt
+        cryptography
+      ];
+
+      doCheck = false;
+    };
+in
+  lib.mkIf config.squirmy.yubikey.enable {
+    home.packages = with pkgs; [
+      # YubiKey Manager
+      # Cross-platform application for configuring any YubiKey over all USB interfaces
+      # https://www.yubico.com/support/download/yubikey-manager/
+      yubikey-manager
+
+      # openssh-key-parser
+      # Utilities to parse and pack OpenSSH private and public key files.
+      # https://github.com/scottcwang/openssh_key_parser
+      # Why: Mentioned on https://developers.yubico.com/SSH/Securing_SSH_with_FIDO2.html
+      # for viewing the contents of the private key.
+      # Usage: python -m openssh_key ~/.ssh/id_ed25519_sk
+      (pkgs.python3.withPackages (python-pkgs: [
+        openssh-key-parser
+      ]))
     ];
-  };
-
-  # Use a yubikey for ssh auth
-  home.sessionVariables = {SSH_AUTH_SOCK = "/tmp/yubikey-agent.sock";};
-}
+  }

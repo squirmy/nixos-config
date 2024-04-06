4c275d69509318dcbdeb06d29d9fd1bff6d01677
Adam Woods <squirmy.dev@gmail.com>
2024-04-06 23:09:27 +1100

    chore: dont run nixos install in bash -c

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@
     else
       print_installing "$description"
       # NixOS multi-user installation
-      bash -c "$(curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm)"
+      curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 
       # Get nix into the path
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then

d4a2b1213214922c1f285f2a02ca08e3b92cdd55
Adam Woods <squirmy.dev@gmail.com>
2024-04-06 16:41:14 +1100

    chore: move configuration convention into nix-machine

--- configuration/default.nix
+++ configuration/default.nix
@@ -1,28 +1,6 @@
-{lib, ...}: let
-  enumerateModules = basePath: fileName: let
-    inherit (builtins) readDir;
-    inherit (lib) attrNames filter filterAttrs pathExists foldl';
-    inherit (lib.trivial) pipe;
-
-    packagePaths = path: attrNames (filterAttrs (_: type: type == "directory") (readDir path));
-    modulesInPackage = package: [package "${fileName}.nix"];
-    renderPath = foldl' (path: elem: path + "/${elem}");
-
-    modules = pipe (packagePaths basePath) [
-      (map modulesInPackage)
-      (map (renderPath basePath))
-      (filter pathExists)
-    ];
-  in
-    {...}: {
-      imports = modules;
-    };
-
-  createModule = enumerateModules ./.;
-in {
+{...}: {
   nix-machine.configurations.squirmy = {
-    options = createModule "options";
-    nixDarwin = createModule "darwin-module";
-    homeManager = createModule "hm-module";
+    path = ./.;
+    scheme = "flat";
   };
 }

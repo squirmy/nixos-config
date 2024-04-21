3053ad3cd26bb930bd986b1e78ce2e46dac97c4b
Adam Woods <squirmy.dev@gmail.com>
2024-04-21 20:48:23 +1000

    chore: update flake inputs (podman broke)

--- configuration/experiments/hm-module.nix
+++ configuration/experiments/hm-module.nix
@@ -1,15 +1,14 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 lib.mkIf config.squirmy.experiments.enable {
-  home.packages = with pkgs; [
-    # playing with getting containers running
-    podman
-    lima
-  ];
+  # home.packages = with pkgs; [
+  #   # playing with getting containers running
+  #   podman
+  #   lima
+  # ];
 
   programs.zoxide = {
     enable = true;

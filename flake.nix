4eeae7107b4dd72af54060db6c932c8fb0af8832
Adam Woods <squirmy.dev@gmail.com>
2024-05-01 00:42:09 +1000

    chore: remove x86

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["aarch64-darwin" "x86_64-darwin"];
+      systems = ["aarch64-darwin"];
       imports = [
         inputs.devshell.flakeModule
         inputs.treefmt-nix.flakeModule

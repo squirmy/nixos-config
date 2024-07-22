8b6a7c7af3c36e7cca8714febbcb8f9ecd9d71cb
Adam Woods <squirmy.dev@gmail.com>
2024-07-22 11:31:21 +1000

    chore: remove automatic key adding

--- configuration/ssh/hm-module.nix
+++ configuration/ssh/hm-module.nix
@@ -7,7 +7,6 @@
 lib.mkIf config.squirmy.ssh.enable {
   programs.ssh.enable = true;
   programs.ssh.package = pkgs.openssh;
-  programs.ssh.addKeysToAgent = "yes";
   programs.ssh.matchBlocks = {
     "github.com" = {
       identityFile = "/users/${config.nix-machine.username}/.ssh/id_ed25519_sk_rk_no-pin-required";

14c002ce333ba3c82822f4da5884f963cd673290
Adam Woods <squirmy.dev@gmail.com>
2024-08-10 14:03:31 +1000

    chore: remove teams

--- configuration/experiments/hm-module.nix
+++ configuration/experiments/hm-module.nix
@@ -9,7 +9,6 @@ lib.mkIf config.squirmy.experiments.enable {
     pkgs.discord
     pkgs.devenv
     pkgs.alt-tab-macos
-    pkgs.teams
     # todo; switch when this becomes available (https://github.com/NixOS/nixpkgs/pull/313973)
     # pkgs.teams-for-linux
 

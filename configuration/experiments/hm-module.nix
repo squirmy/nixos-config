fd57099ea886a57ed183aada342ebfdf9aa5e1ec
Adam Woods <squirmy.dev@gmail.com>
2024-10-17 23:15:43 +1100

    chore: remove alttab

--- configuration/experiments/hm-module.nix
+++ configuration/experiments/hm-module.nix
@@ -7,7 +7,7 @@
 lib.mkIf config.squirmy.experiments.enable {
   home.packages = [
     pkgs.discord
-    pkgs.alt-tab-macos
+    # pkgs.alt-tab-macos
     # todo; switch when this becomes available (https://github.com/NixOS/nixpkgs/pull/313973)
     # pkgs.teams-for-linux
   ];

ac6440a4c1d2ce2b07dbbc139615639cfe70b709
Adam Woods <squirmy.dev@gmail.com>
2024-11-05 17:08:42 +1100

    feat: add frequently used project packages for convenience

--- configuration/experiments/hm-module.nix
+++ configuration/experiments/hm-module.nix
@@ -3,12 +3,22 @@
   config,
   pkgs,
   ...
-}:
-lib.mkIf config.squirmy.experiments.enable {
-  home.packages = [
-    pkgs.discord
-    # pkgs.alt-tab-macos
-    # todo; switch when this becomes available (https://github.com/NixOS/nixpkgs/pull/313973)
-    # pkgs.teams-for-linux
-  ];
-}
+}: let
+  beam_pkgs = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
+in
+  lib.mkIf config.squirmy.experiments.enable {
+    home.packages = [
+      pkgs.discord
+
+      # pkgs.alt-tab-macos
+      # todo; switch when this becomes available (https://github.com/NixOS/nixpkgs/pull/313973)
+      # pkgs.teams-for-linux
+
+      # Convenience
+      pkgs.colima
+      pkgs.docker
+      pkgs.go
+      beam_pkgs.elixir
+      beam_pkgs.erlang
+    ];
+  }

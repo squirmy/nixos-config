343e24310755a52d1947e50d04757ca2db07897d
Adam Woods <squirmy.dev@gmail.com>
2024-11-18 18:11:18 +1100

    chore: update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   inputs = {
-    # Back to unstable after: https://github.com/LnL7/nix-darwin/pull/1179
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/8809585e6937d0b07fc066792c8c9abf9c3fe5c4";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";

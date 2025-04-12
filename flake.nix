5d6a2668225a6a13cb5abaf994ce93c9222f0421
Adam Woods <squirmy.dev@gmail.com>
2025-04-12 02:01:45 +0000

    chore: add linux as a target

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["aarch64-darwin"];
+      systems = ["aarch64-darwin" "x86_64-linux"];
       imports = [
         inputs.devshell.flakeModule
         inputs.treefmt-nix.flakeModule
